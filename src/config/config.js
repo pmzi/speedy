403c292acbfd1e466f2e61f6411ab131ce3ae5ec
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-12-14 17:09:35 +0330

    config file now contains more data

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,12 @@ module.exports = {
     interpolating: {
         url: 'https://www.dcode.fr/api/',
         tool: 'lagrange-interpolating-polynomial'
+    },
+    testRange:{
+        from: 0,
+        to: 5
+    },
+    general: {
+        github: 'https://github.com/pmzi/speedy'
     }
 }
